afe238ba9f137ba2cef9cfd4a1c49404fa0bdf3d
ADMIN <ADMIN@ATOMICBOMB>
2024-05-09 19:37:54 +0300

    Добавьте файлы проекта.

---
+++ TA/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lstTokens = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.FormattingEnabled = true;
+            this.txtInput.ItemHeight = 20;
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(387, 384);
+            this.txtInput.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lstTokens
+            // 
+            this.lstTokens.FormattingEnabled = true;
+            this.lstTokens.ItemHeight = 20;
+            this.lstTokens.Location = new System.Drawing.Point(486, 12);
+            this.lstTokens.Name = "lstTokens";
+            this.lstTokens.Size = new System.Drawing.Size(302, 384);
+            this.lstTokens.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstTokens);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox txtInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lstTokens;
+    }
+}
+
---
+++ TA/Form1.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = txtInput.Text;
+            List<Token> tokens = Analyzer.Analyze(inputText);
+            lstTokens.Items.Clear();
+            foreach (Token token in tokens)
+            {
+                lstTokens.Items.Add("( " + token.Type + " " + token.Name + " )");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files |*.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                string inputText = File.ReadAllText(fileName);
+                txtInput.Text = inputText;
+            }
+        }
+
+        public class Token
+        {
+            public string Type { get; set; }
+            public string Name { get; set; }
+            public Token(string type, string name)
+            {
+                Type = type;
+                Name = name;
+            }
+        }
+
+        public static class Analyzer
+        {
+            private static readonly string[] keywords = { "int", "string", "double", "if", "else", "main" };
+            private static readonly string[] operators = { "+", ">", "<", "=", "*", "/"};
+            public static List<Token> Analyze(string inputText)
+            {
+                List<Token> tokens = new List<Token>();
+                string currentWord = "";
+                for (int i = 0; i < inputText.Length; i++)
+                {
+                    char currentChar = inputText[i];
+                    if (char.IsLetterOrDigit(currentChar) || currentChar == '_')
+                    {
+                        currentWord += currentChar;
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrWhiteSpace(currentWord))
+                        {
+                            if(currentWord == "int" || currentWord == "string" || currentWord == "double" || currentWord == "if" || currentWord == "else" || currentWord == "main")
+                            {
+                                string type = GetTokenType(currentWord);
+                                tokens.Add(new Token(type, ""));
+                                currentWord = "";
+                            }
+                            else
+                            {
+                                string type = GetTokenType(currentWord);
+                                tokens.Add(new Token(type, currentWord));
+                                currentWord = "";
+                            }
+                        }
+                        if (!char.IsWhiteSpace(currentChar))
+                        {
+                            string type = GetSymbolType(currentChar);
+                            tokens.Add(new Token(type, ""));
+                        }
+                    }
+                }
+
+                string types = String.Join(" ", tokens.Select(t => t.Type));
+                string[] words = types.Split(' ');
+
+                Program(words, 0);
+
+                return tokens;
+            }
+
+            private static void Program(string[] words, int current)
+            {
+                Main(words, current);
+            }
+
+            private static void Main(string[] words, int current)
+            {
+                if (words[current] == "main")
+                {
+                    OS(words, current+1);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось main");
+                }
+            }
+
+            private static void OS(string[] words, int current)
+            {
+                if (words[current] == "(")
+                {
+                    ZS(words, current + 1);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось (");
+                }
+            }
+
+            private static void ZS(string[] words, int current)
+            {
+                if (words[current] == ")")
+                {
+                    FOS(words, current + 1);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось )");
+                }
+            }
+
+            private static void FOS(string[] words, int current)
+            {
+                if (words[current] == "{")
+                {
+                    SB(words, current + 1);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось {" + current);
+                }
+            }
+
+            private static void SB(string[] words, int current)
+            {
+                if(SO(words, current))
+                {
+                    SP(words, current + 1);
+                }
+                else if(SOP(words, current))
+                {
+                    KOP(words, current);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось описание или операция");
+                }
+            }
+
+            private static void KOP(string[] words, int current)
+            {
+                if (words[current] == "if")
+                {
+                    UOP(words, current + 1);
+                }
+                if (words[current] == "else")
+                {
+                    FOS(words, current + 1);
+                }
+                if (words[current] == "id")
+                {
+                    OPP(words, current + 1);
+                }
+            }
+
+            private static void OPP(string[] words, int current)
+            {
+                if (words[current] == "=")
+                {
+                    EXPR1(words, current + 1);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось =");
+                }
+            }
+
+            private static void EXPR1(string[] words, int current)
+            {
+                for (int i = current; i < words.Length; i++)
+                {
+                    if (words[i] == ";")
+                    {
+                        current = i;
+                        KP(words, current + 1);
+                        break;
+                    }
+                }
+            }
+
+            private static void UOP(string[] words, int current)
+            {
+                if (words[current] == "(")
+                {
+                    EXPR(words, current);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось (");
+                }
+            }
+
+            public static void EXPR(string[] words, int current)
+            {
+                Dictionary<string, int> priority = new Dictionary<string, int>
+                {
+                    { "(", 0 },
+                    { ")", 1 },
+                    { "&", 2 },
+                    { "<", 3 },
+                    { ">", 4 },
+                    { "=", 5 },
+                    { "+", 6 },
+                    { "-", 7 },
+                    { "*", 8 },
+                    { "/", 9 }
+                };
+
+                Stack<string> operatorsStack = new Stack<string>();
+                string output = " ";
+
+                for (int i = current; i < words.Length; i++)
+                {
+                    if (words[i] == "id" || words[i] == "lit")
+                    {
+                        output += words[i] + " ";
+                    }
+                    else if (words[i] == "(")
+                    {
+                        operatorsStack.Push(words[i]);
+                    }
+                    else if (words[i] == ")")
+                    {
+                        while (operatorsStack.Peek() != "(")
+                        {
+                            output += operatorsStack.Pop() + " ";
+                        }
+                        operatorsStack.Pop();
+
+                        if (words[i+1] == "{")
+                        {
+                            current = i;
+                            FOS(words, current + 1);
+                            break;
+                        }
+                        if (words[i+1] == "int" || words[i+1] == "string" || words[i+1] == "double" || words[i+1] == "id" || words[i+1] == "if")
+                        {
+                            current = i;
+                            SB(words, current + 1);
+                            break;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        if (operatorsStack.Count > 0 && priority[words[i]] <= priority[operatorsStack.Peek()])
+                        {
+                            output += operatorsStack.Pop() + " ";
+                        }
+                        operatorsStack.Push(words[i]);
+                    }
+                }
+
+                while (operatorsStack.Count > 0)
+                {
+                    output += operatorsStack.Pop() + " ";
+                }
+
+                if (CheckReversePolishNotation(output))
+                {
+                    MessageBox.Show("Выражение записано верно.");
+                }
+                else
+                {
+                    MessageBox.Show("Выражение записано неверно.");
+                }
+            }
+
+            private static bool CheckReversePolishNotation(string input)
+            {
+                string[] tokens = input.Split(' ');
+
+                Stack<string> stack = new Stack<string>();
+
+                foreach (string token in tokens)
+                {
+                    if (token == ">" || token == "<" || token == "=")
+                    {
+                        if (stack.Count < 2)
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                        stack.Pop();
+                    }
+                    if (token == "id" || token == "lit")
+                    {
+                        stack.Push(token);
+                    }
+                }
+                return stack.Count == 0; 
+            }
+
+            private static bool SO(string[] words, int current)
+            {
+                if (words[current] == "int" || words[current] == "string" || words[current] == "double")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            private static bool SOP(string[] words, int current)
+            {
+                if (words[current] == "if" || words[current] == "id" || words[current] == "else")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            private static void SP(string[] words, int current)
+            {
+                if (words[current] == "id")
+                {
+                    SPZ(words, current + 1);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидался идентификатор");
+                }
+            }
+
+            private static void SPZ(string[] words, int current)
+            {
+                if (words[current] == ",")
+                {
+                    SP(words, current + 1);
+                }
+                else if (words[current] == ";")
+                {
+                    KP(words, current + 1);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось , или ;");
+                }
+            }
+
+            private static void KP(string[] words, int current)
+            {
+                if (words[current] == "}")
+                {
+                    if (current == words.Length - 1)
+                    {
+                        MessageBox.Show("Синтаксиз завершен успешно завершен (ошибок нет)");
+                    }
+                    else
+                    {
+                        KP(words, current + 1);
+                    }
+                }
+                else if (words[current] == "int" || words[current] == "string" || words[current] == "double" || words[current] == "id" || words[current] == "if" || words[current] == "else")
+                {
+                    SB(words, current);
+                }
+                else
+                {
+                    MessageBox.Show("Встречено " + words[current] + " а ожидалось описание, операция или }");
+                }
+            }
+
+            private static string GetTokenType(string word)
+            {
+                if (IsKeyword(word))
+                {
+                    return word;
+                }
+                else if (IsNumber(word))
+                {
+                    return "lit";
+                }
+                else if (IsVariable(word))
+                {
+                    return "id";
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            private static string GetSymbolType(char symbol)
+            {
+                if (operators.Contains(symbol.ToString()))
+                {
+                    return symbol.ToString();
+                }
+                else
+                {
+                    return symbol.ToString();
+                }
+            }
+            private static bool IsKeyword(string word)
+            {
+                if(word.Length <= 8)
+                {
+                    return keywords.Contains(word);
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                    return false;
+                }
+            }
+            private static bool IsNumber(string word)
+            {
+                int num;
+                return int.TryParse(word, out num);
+            }
+            private static bool IsVariable(string word)
+            {
+                if (word.Length <= 8)
+                {
+                    return char.IsLetter(word[0]);
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                    return false;
+                }
+            }
+
+            private static bool IsOperator(string word)
+            {
+                return operators.Contains(word);
+            }
+        }
+    }
+}
